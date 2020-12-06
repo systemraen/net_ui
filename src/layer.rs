2de8d54f16182303290896893f3220418327c929
xipi2048 <raen@quantumtrip.org>
2020-12-06 14:09:48 -0800

    Remove add_widget()

--- src/layer.rs
+++ src/layer.rs
@@ -2,7 +2,7 @@ use crate::widgets::prelude::Widget;
 
 #[derive(Default)]
 pub struct Layer {
-	widgets: Vec<Box<&'static Widget>>
+	pub widgets: Vec<Box<&'static Widget>>
 }
 
 impl Layer {
@@ -10,8 +10,5 @@ impl Layer {
 		Layer {
 			widgets: vec![]
 		}
-	}
-	pub fn add_widget<T>(&mut self, widget: &'static T) where T: Widget {
-		self.widgets.push(Box::new(widget));
 	}
 }
