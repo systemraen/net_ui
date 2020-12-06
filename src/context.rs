74febb7871daa29c9268ea1f69ae5162d374f4eb
xipi2048 <raen@quantumtrip.org>
2020-12-06 14:46:51 -0800

    Use submod version of quicksilver

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,4 @@
-use crate::layer::Layer;
+use { crate::layer::Layer, quicksilver::graphics::Graphics };
 
 pub struct Context {
 	layers: Vec<&'static Layer>,
@@ -8,7 +8,16 @@ impl Context {
 	pub fn new() -> Self {
 		Context { layers: vec![] }
 	}
+
 	pub fn add_layer(&mut self, layer: &'static Layer) {
 		self.layers.push(layer);
+	}
+
+	pub fn clear_layers(&mut self) {
+		self.layers.clear();
+	}
+
+	pub fn draw(&self, gfx: &mut Graphics) {
+
 	}
 }
